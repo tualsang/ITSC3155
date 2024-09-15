567ffd74a1c0f9fa31fa14410deab96cd896014b
Tuan Sang <tsang3@uncc.edu>
2024-09-15 09:44:52 -0400

    feat: added make_sandwich function

--- main.py
+++ main.py
@@ -74,5 +74,8 @@ class SandwichMachine:
     def make_sandwich(self, sandwich_size, order_ingredients):
         """Deduct the required ingredients from the resources.
            Hint: no output"""
+        for item, quantity in order_ingredients.items():
+            self.machine_resources[item] -= quantity
+        print(f"{sandwich_size} sandwich is ready. Bon appetit!")
 
 ### Make an instance of SandwichMachine class and write the rest of the codes ###
