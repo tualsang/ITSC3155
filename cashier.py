902f95266f8fde89c7763793bfcfe507f0c6800e
Tuan Sang <tsang3@uncc.edu>
2024-09-17 11:56:43 -0400

    feat: Made the cashier function into a module.

---
+++ cashier.py
@@ -0,0 +1,24 @@
+class Cashier:
+    def __init__(self):
+        pass
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted.
+           Hint: include input() function here, e.g. input("how many quarters?: ")"""
+        print("Please insert coins.")
+        total = int(input("How many large dollars? "))
+        total += int(input("How many half dollars?: ")) * 0.50
+        total += int(input("How many quarters?: ")) * 0.25
+        total += int(input("How many nickels?: ")) * 0.05
+        return total
+
+    def transaction_result(self, coins, cost):
+        """Return True when the payment is accepted, or False if money is insufficient.
+           Hint: use the output of process_coins() function for cost input"""
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            print(f"Here is ${change} in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
