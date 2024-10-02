c77ef7a10fe91847007e80321b2026c207bb07e6
Tuan Sang <tsang3@uncc.edu>
2024-10-02 18:52:15 -0400

    feat: FastAPI Assignment Final Commit

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
-import data
-from sandwich_maker import SandwichMaker
-from cashier import Cashier
+from typing import Union
 
-# Make an instance of other classes here
-resources = data.resources
-recipes = data.recipes
-sandwich_maker_instance = SandwichMaker(resources)
-cashier_instance = Cashier()
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-def main():
-    ###  write the rest of the codes ###
-    while True:
-        userChoice = input("What would you like? (small/ medium/ large/ off/ report): ").lower()
-        if userChoice == "off":
-            break
-        elif userChoice == "report":
-            for item, quantity in sandwich_maker_instance.machine_resources.items():
-                print(f"{item.capitalize()}: {quantity}")
-            break
-        elif userChoice in ["small", "medium", "large"]:
-            sandwich = recipes[userChoice]
-            if sandwich_maker_instance.check_resources(sandwich["ingredients"]):
-                payment = cashier_instance.process_coins()
-                if cashier_instance.transaction_result(payment, sandwich["cost"]):
-                    sandwich_maker_instance.make_sandwich(userChoice, sandwich["ingredients"])
-            else:
-                print(f"Sorry not enough ingredients.")
-        else:
-            print("Invalid input.")
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
-if __name__ == "__main__":
-    main()
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
